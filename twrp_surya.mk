040197818e59dfe44779d1b3dd3f6493b8a5bd0f
infinixs5lite <kelvinchinedu97@gmail.com>
2024-05-19 21:52:07 +0100

    Update twrp_surya.mk

--- twrp_surya.mk
+++ twrp_surya.mk
@@ -1,33 +1,28 @@
 #
-# Copyright 2018 The Android Open Source Project
-# Copyright 2014-2022 The Team Win LLC
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# The below variables will be generated automatically
-#
-# Release name (automatically taken from this file's suffix)
-PRODUCT_RELEASE_NAME := $(lastword $(subst /, ,$(lastword $(subst _, ,$(firstword $(subst ., ,$(MAKEFILE_LIST)))))))
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Custom vendor used in build tree (automatically taken from this file's prefix)
-CUSTOM_VENDOR := $(lastword $(subst /, ,$(firstword $(subst _, ,$(firstword $(MAKEFILE_LIST))))))
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/$(CUSTOM_VENDOR)/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# OEM Info (automatically taken from device tree path)
-BOARD_VENDOR := $(or $(word 2,$(subst /, ,$(firstword $(MAKEFILE_LIST)))),$(value 2))
+# Inherit from surya device
+$(call inherit-product, device/xiaomi/surya/device.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
-PRODUCT_NAME := $(CUSTOM_VENDOR)_$(PRODUCT_RELEASE_NAME)
+PRODUCT_DEVICE := surya
+PRODUCT_NAME := twrp_surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
-PRODUCT_MANUFACTURER := $(BOARD_VENDOR)
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_RELEASE_NAME := surya
 
-# Device path for OEM device tree
-DEVICE_PATH := device/$(BOARD_VENDOR)/$(PRODUCT_DEVICE)
-
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
